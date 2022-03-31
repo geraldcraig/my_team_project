f1d667309531364bbacdc5a3be1fe56ac6a98a99
Gerald Craig <geraldcraig@me.com>
2022-03-31 16:12:03 +0100

    gitlab test added

---
+++ Gitlab Test/src/gitlab/test/Greeting.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gitlab.test;
+
+/**
+ * @author Gerald
+ *
+ */
+public class Greeting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
